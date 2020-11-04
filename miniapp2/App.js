539a1b663dabc61f0702f0c643bcb815c4dc15ad
Ye Na Rhee <yenarhee@sg.ibm.com>
2020-11-04 22:57:27 +0800

    miniapp2 (react native 0.59)

---
+++ miniapp2/App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  setActiveBundle,
+  registerBundle,
+  reloadBundle,
+  getActiveBundle,
+  getBundles,
+} from 'react-native-dynamic-bundle';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    console.log(React.version);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button onPress={this._onBackButtonPress} title="Back" />
+      </View>
+    );
+  }
+
+  _onBackButtonPress = async () => {
+    setActiveBundle(null);
+    console.log('setActiveBundle');
+
+    const bundles = await getBundles();
+    console.log(bundles);
+
+    const activeBundle = await getActiveBundle();
+    console.log(activeBundle);
+
+    reloadBundle();
+    console.log('reloadBundle');
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
